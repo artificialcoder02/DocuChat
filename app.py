d9ce61afe7039d1709634885957eae7756cd5613
ArtificialCoder-02 <rc.tuhin02@gmail.com>
2023-09-01 16:49:13 +0530

    Made Changes

--- app.py
+++ app.py
@@ -1,26 +1,42 @@
 import chainlit as cl
 
 import os
-huggingfacehub_api_token = os.environ['HUGGINGFACEHUB_API_TOKEN']
+#huggingfacehub_api_token = os.environ['HUGGINGFACEHUB_API_TOKEN']
 
 from langchain import HuggingFaceHub, PromptTemplate, LLMChain
 
 repo_id = "tiiuae/falcon-7b-instruct"
-llm = HuggingFaceHub(huggingfacehub_api_token=huggingfacehub_api_token, 
+llm = HuggingFaceHub(huggingfacehub_api_token="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", 
                      repo_id=repo_id, 
                      model_kwargs={"temperature":0.6, "max_new_tokens":2000})
 
 template = """
-You are an AI assistant, you need to read and analyse the query and answer them in the best way possible
+I want you to act as a Artificial Intelligence personal assistance agent. 
 
 {question}
 
 """
 
 
-@cl.langchain_factory
-def factory():
+@cl.on_chat_start
+def main():
+    # Instantiate the chain for that user session
     prompt = PromptTemplate(template=template, input_variables=["question"])
     llm_chain = LLMChain(prompt=prompt, llm=llm, verbose=True)
 
-    return llm_chain
+    # Store the chain in the user session
+    cl.user_session.set("llm_chain", llm_chain)
+
+
+@cl.on_message
+async def main(message: str):
+    # Retrieve the chain from the user session
+    llm_chain = cl.user_session.get("llm_chain")  # type: LLMChain
+
+    # Call the chain asynchronously
+    res = await llm_chain.acall(message, callbacks=[cl.AsyncLangchainCallbackHandler()])
+
+    # Do any post processing here
+
+    # Send the response
+    await cl.Message(content=res["text"]).send()
